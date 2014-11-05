36e8a94588ce121b4eb76848c79b8188a2a985d6
expir3dcow <expir3dcow@gmail.com>
2014-11-05 15:23:03 +0000

    Update main.cpp

--- use_controller/main.cpp
+++ use_controller/main.cpp
@@ -1,14 +1,10 @@
-
-#include <string>
 #include <jni.h>
-#include <string.h>
 #include <android/log.h>
 #include <dlfcn.h>
 #include "mcpelauncher.h"
 
-using namespace std; //so we don't need to say std::string
 
-static int(*UseController_Real)(); //this is the real mcpe function, p is a string , void* can point to any data type
+static int(*UseController_Real)(); //this is the real mcpe function
 
 int useControllerHook(){ //this is the function that will override the real one	
 	__android_log_print(ANDROID_LOG_INFO, "MyMod", "Swag level is over 9000");
@@ -20,7 +16,7 @@ int useControllerHook(){ //this is the function that will override the real one
 
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-	void *handle = dlsym(RTLD_DEFAULT,"_ZN9Minecraft13useControllerEv"); //this is the symbol for random splash
+	void *handle = dlsym(RTLD_DEFAULT,"_ZN9Minecraft13useControllerEv"); //this is the symbol for using the controller
 	mcpelauncher_hook(handle,(void*)&useControllerHook,(void**)&UseController_Real); //we hook the method
 	return JNI_VERSION_1_2;
 }
